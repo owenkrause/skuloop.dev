385325658f6ce8218abc6cd0592e55ab89fc18f2
Owen Krause <owenjkrause@gmail.com>
2022-03-06 14:43:58 -0800

    finished server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'html');
 app.get('', (req, res) => {
-  res.render('index');
+  res.sendFile(__dirname + '/index.html');
 })
 
 app.listen(port, () => {
