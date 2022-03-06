ccfbcea2255ac17245dffde96ba4c242e28a3540
Owen Krause <owenjkrause@gmail.com>
2022-03-06 14:32:21 -0800

    server changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('express')();
 
 const port = process.env.PORT || 80;
 
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'html');
 app.get('', (req, res) => {
   res.render('index');
